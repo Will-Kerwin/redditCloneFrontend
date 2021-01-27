e6866a0a969de3a291197c2a11732ed191572eb3
Will Kerwin <will@itskerwin.co.uk>
2021-01-27 17:20:34 +0000

    Initial commit + not a fan of ang i dont think but hey we will become a fan of it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SignUpComponent} from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
