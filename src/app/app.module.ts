465863a290f5b8786f0c3248db531b1a2129f9d6
Will Kerwin <will@itskerwin.co.uk>
2021-01-28 17:45:21 +0000

    implemented login and signup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignUpComponent } from './auth/signup/signup.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import {NgxWebstorageModule} from 'ngx-webstorage';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignUpComponent
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxWebstorageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {SignupRequestPayload} from './signup-request.payload';
+import {AuthService} from '../shared/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,16 +10,33 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
 
+  signupRequestPayload: SignupRequestPayload;
   signupForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.email]),
-      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
     });
   }
 
+  signup(): void {
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
 }
